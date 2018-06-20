2d42a7a7cd7b4f9ca39e81d012c4ffb70eb299cc
Инсаф Мухаметшин <IMukhametshin@alfabank.ru>
2018-06-20 10:59:06 +0300

    Create project

---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Инсаф Мухаметшин on 20.06.2018.
+//  Copyright © 2018 HOME. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
